79ceb248395efc12935075524158f5c8ab2fd99a
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-06-10 12:32:23 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "modern-normalize";
-import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
