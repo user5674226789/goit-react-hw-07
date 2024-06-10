daf577fe29adb0de637c0a630a690e8c1067c784
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-06-10 08:48:11 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import modern-normalize from "modern-normalize";
+import "modern-normalize";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
